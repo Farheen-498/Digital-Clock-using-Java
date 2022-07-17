7d9947baa8ea79ccdd910d062833297cfd166f08
Farheen Ali <65396038+Farheen-498@users.noreply.github.com>
2022-07-17 23:10:37 +0530

    Add files via upload

---
+++ com.farheen.digitalClock/src/com/farheen/Main.java
@@ -0,0 +1,13 @@
+package com.farheen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("TIMER STARTED!!");
+
+		MyWindow wind= new MyWindow("Time");
+		
+		
+	}
+
+}
